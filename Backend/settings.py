70b15e6a77f40213f16f5ad9d3785c3fdce7bdd9
reeplayed98 <lukasz.borkowski8991@gmail.com>
2020-05-19 02:14:22 +0200

    'f'

--- Backend/settings.py
+++ Backend/settings.py
@@ -137,6 +137,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
-CORS_ORIGIN_WHITELIST = ['https://jolly-ride-e2812e.netlify.app', 'http://localhost:8080','https://naughty-khorana-bd87a0.netlify.com']
+CORS_ORIGIN_WHITELIST = ['https://jolly-ride-e2812e.netlify.app', 'http://localhost:8080','https://naughty-khorana-bd87a0.netlify.app']
 
 django_heroku.settings(locals())
